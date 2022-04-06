daaf2dbe254b397d82ad38f78a05978c3fb2d7fa
Ikromjon88 <ikromjonqurbonov86@gmail.com>
2022-04-06 06:10:17 +0500

    yangi

--- chart/core/models.py
+++ chart/core/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Post(models.Model):
+    class Meta:
+        verbose_name="Post"
+        verbose_name_plural="Postlar"
     nomi=models.CharField(max_length=50)
+    daromadi=models.IntegerField()
+
+    def __str__(self):
+        return self.nomi 
